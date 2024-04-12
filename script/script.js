04cfd3f966e2240a0e84ad02f6699013f9706a0a
Juan Jose Gonzalez Quintero <juan.jgq@toc.principal>
2024-04-12 06:12:43 -0500

    Correcciones primera revision sprint 7

--- script/script.js
+++ script/script.js
@@ -103,6 +103,7 @@ addButton.addEventListener('click',function(){
   popupButton.classList.add('popup__close-enabled');
   popUpContainer.classList.add('popup__enabled');
   popUpContainer.querySelector('.popup__overlay').addEventListener('click',closeModal);
+  saveCard.disabled = true;
 
 });
 
@@ -135,11 +136,11 @@ edit.addEventListener('click', function(){
   editWindow.classList.add('edit__enabled');
   editButton.classList.add('edit__close-enabled');
   editForm.classList.add('edit__window-enabled');
-  console.log('edicion habilitada');
   editWindow.querySelector('.edit__overlay').addEventListener('click',closeModal);
   name.value = explorerName;
   job.value = explorerJob;
   popUpContainer.querySelector('.popup__overlay').addEventListener('click',closeModal);
+  saveEdit.disabled = true;
 });
 
 editButton.addEventListener('click', function(){
@@ -184,89 +185,5 @@ function editExplorer(){
 saveCard.addEventListener('click', addCard);
 saveEdit.addEventListener('click', editExplorer);
 
-
-/**
- * Manejo de validaciones
-
-*/
-
-// elimina las clases de error de los span segun su nombre
-const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove('form__input_type_error');
-  errorElement.classList.remove('form__input-error_active');
-  errorElement.textContent = '';
-};
-
-// agrega las clases de error a los span segun su id
-const showInputError = (formElement, inputElement, errorMessage, config) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  console.log(formElement);
-  inputElement.classList.add(config.inputErrorClass);
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add('form__input-error_active');
-};
-
-//Valida si el elemento es valido usuando su contenedor(formulario) y su elemento
-const checkInputValidity = (formElement, inputElement, config) => {
-  if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage, config);
-  } else {
-    hideInputError(formElement, inputElement);
-  }
-};
-
-// Verifica si por lo menos uno de los elementos es invalido
-const hasInvalidInput = (inputList) => {
-  return inputList.some((inputElement) => {
-    return !inputElement.validity.valid;
-  });
-};
-
-//Activa o inactiva el botón de acuerdo a la valides de todos los input
-const toggleButtonState = (inputList, buttonElement) => {
-  if (hasInvalidInput(inputList)) {
-    buttonElement.classList.add('button_inactive');
-    buttonElement.removeEventListener('click',addCard);
-    buttonElement.removeEventListener('click',editExplorer);
-
-  } else {
-    buttonElement.classList.remove('button_inactive');
-    saveCard.addEventListener('click', addCard);
-    saveEdit.addEventListener('click', editExplorer);
-  }
-};
-
-const setEventListeners = (inputList, formElement, config) => {
-  const buttonElement = formElement.querySelector(config.submitButtonSelector);
-  toggleButtonState(inputList, buttonElement);
-
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener('input', function () {
-      checkInputValidity(formElement, inputElement, config);
-      toggleButtonState(inputList, buttonElement);
-    });
-  });
-};
-
-const enableValidation = (config) => {
-  const formList = Array.from(document.querySelectorAll(config.formSelector));
-  formList.forEach((formElement) => {
-    formElement.addEventListener('submit', function (evt) {
-      evt.preventDefault();
-    });
-    const fieldsetList = Array.from(formElement.querySelectorAll(config.inputSelector));
-    setEventListeners(fieldsetList, formElement, config);
-  });
-};
-
-enableValidation({
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__button',
-  inactiveButtonClass: 'popup__button_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
-});
 
 
